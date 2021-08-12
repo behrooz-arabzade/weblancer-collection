346c572f901cd0d6220159263c45087af626e6e5
unknown <behrooz.arabzade@gmail.com>
2021-08-12 11:50:23 +0430

    set some logging

--- src/datatypes/File.js
+++ src/datatypes/File.js
@@ -2,66 +2,6 @@ let Sequelize = require('sequelize');
 const {DataTypes} = Sequelize;
 
 module.exports.define = function define(Sequelize) {
-
 }
 
-class FILE extends DataTypes.ABSTRACT {
-    toSql() {
-        return 'JSONB';
-    }
-
-    // Optional, validator function
-    // {
-    //     urlPath: "1/2/3/asdd/asdasd.png",
-    //     extension: "png",
-    //     name: "asdasd.png",
-    //     type: "image"
-    // }
-    validate(value, options) {
-        if (typeof value === 'object') {
-            return (value.urlPath);
-        }
-
-        return false;
-    }
-
-    _sanitize(value) {
-        // Force all numbers to be positive
-        if (typeof value === "string") {
-            try {
-                value = JSON.parse(value);
-            } catch {
-                value = {
-                    url: value
-                }
-            }
-        }
-        if (!value.type) {
-            value.type = value.urlPath.split('.').pop();
-        }
-        if (!value.name) {
-            value.name = value.urlPath.substr(value.urlPath.lastIndexOf("/") + 1);
-        }
-        if (!value.type) {
-            value.type = "other";
-        }
-
-        return value;
-    }
-
-    _stringify(value) {
-        return JSON.stringify(value);
-    }
-
-    static parse(value) {
-        return JSON.parse(value);
-    }
-}
-
-DataTypes.FILE = FILE;
-
-DataTypes.FILE.prototype.key = DataTypes.FILE.key = 'FILE'
-
-Sequelize.FILE = Sequelize.Utils.classToInvokable(DataTypes.FILE);
-
-module.exports.FILE = DataTypes.FILE;
+module.exports.FILE = DataTypes.JSONB;
